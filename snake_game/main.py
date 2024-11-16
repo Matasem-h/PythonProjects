8d32eb4ed769a2a0c34c17e1bb6dbcadac97ee14
Matasem <Matasem.h97@gmail.com>
2024-11-16 21:46:41 +0300

    added_keybinds_to_snake_game

--- snake_game/main.py
+++ snake_game/main.py
@@ -11,6 +11,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 is_snake_moving = True
 while is_snake_moving:
     screen.update()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -7,6 +11,7 @@ class Snake:
     def __init__(self):
         self.squares = []
         self.create_snake()
+        self.head = self.squares[0]
 
     def create_snake(self):
         leo_x_position = 0
@@ -25,3 +30,19 @@ class Snake:
             new_y = self.squares[square_num - 1].ycor()
             self.squares[square_num].goto(new_x, new_y)
         self.squares[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
