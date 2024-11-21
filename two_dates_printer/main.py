aa876e9c4a5ecab1fb3a74c0e614a5c844105523
Matasem <Matasem.h97@gmail.com>
2024-11-21 19:52:03 +0300

    added_comments

--- two_dates_printer/main.py
+++ two_dates_printer/main.py
@@ -1,17 +1,17 @@
 # This code can be used to print all dates between two specified dates, this was written by for my personal use.
 from datetime import date, timedelta
 
-# You can enter any two dates here to print all the days between them
+# You can enter any two dates here to print all the days between them.
 start_date = date(2024, 11, 20)
 end_date = date(2025, 6, 30)
 
 delta = end_date - start_date
 
-for i in range(delta.days + 1):  # This for loop will do all the printing between two dates.
+for i in range(delta.days + 1):  # This for loop will do all the printing between the two dates.
     day = start_date + timedelta(days=i)
     if day.weekday() == 4:
-        print(day.strftime("%d.%m.%Y") + " Friday")  # Preferred date format, and only the word Friday next to its date
-    # elif day.weekday() == 5:                       # You can turn this on to print Saturday next to its date 
+        print(day.strftime("%d.%m.%Y") + " Friday")  # Preferred date format, and only the word Friday next to its date.
+    # elif day.weekday() == 5:                       # You can turn this on to print Saturday next to its date.
     #     print(day.strftime("%d.%m.%Y") + " Saturday")
-    else:
+    else:  # This is the text that will be printed next to each date, it can be modified as required.
         print(day.strftime("%d.%m.%Y") + " " + "                                                      | Course:                   | Walk:       | Push-Ups:    | Extra: ")
