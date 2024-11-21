3430332eed03f763fcbe01f78a9c7f824e27fce8
Matasem <Matasem.h97@gmail.com>
2024-11-21 19:50:49 +0300

    added_comments

--- two_dates_printer/main.py
+++ two_dates_printer/main.py
@@ -7,11 +7,11 @@ end_date = date(2025, 6, 30)
 
 delta = end_date - start_date
 
-for i in range(delta.days + 1):
+for i in range(delta.days + 1):  # This for loop will do all the printing between two dates.
     day = start_date + timedelta(days=i)
     if day.weekday() == 4:
-        print(day.strftime("%d.%m.%Y") + " Friday") # This is the date format I personally prefer
-    # elif day.weekday() == 5:
+        print(day.strftime("%d.%m.%Y") + " Friday")  # Preferred date format, and only the word Friday next to its date
+    # elif day.weekday() == 5:                       # You can turn this on to print Saturday next to its date 
     #     print(day.strftime("%d.%m.%Y") + " Saturday")
     else:
         print(day.strftime("%d.%m.%Y") + " " + "                                                      | Course:                   | Walk:       | Push-Ups:    | Extra: ")
