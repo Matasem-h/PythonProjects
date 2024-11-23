9e4058fcddc41c49470189c7a4e2593ee773cb59
Matasem <Matasem.h97@gmail.com>
2024-11-23 19:05:36 +0300

    added_paddle

--- pong_game/main.py
+++ pong_game/main.py
@@ -4,29 +4,32 @@ screen.bgcolor("black")
 screen.setup(height=600, width=800)
 screen.title("Pong")
 
-
-right_player = Turtle()
-right_player.penup()
-right_player.shape("square")
-right_player.shapesize(5, 1)
-right_player.color("white")
+paddle = Turtle()
+paddle.hideturtle()
+paddle.speed("fastest")
+paddle.penup()
+paddle.shape("square")
+paddle.shapesize(5, 1)
+paddle.color("white")
 rp_x_pos = 350
 rp_y_pos = 0
-right_player.goto(rp_x_pos, rp_y_pos)
+paddle.goto(rp_x_pos, rp_y_pos)
+paddle.showturtle()
 
-left_player = Turtle()
-left_player.penup()
-left_player.shape("square")
-left_player.shapesize(5, 1)
-left_player.color("white")
-lp_x_pos = -350
-lp_y_pos = 0
-left_player.goto(lp_x_pos, lp_y_pos)
+
+def move_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def move_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
 
 screen.listen()
-screen.onkey()
-
-
+screen.onkey(key="Up", fun=move_up)
+screen.onkey(key="Down", fun=move_down)
 
 
 screen.exitonclick()
