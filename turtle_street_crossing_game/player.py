56ffaee8275758cb4fd9bf829e8647f3883ad6b5
Matasem <Matasem.h97@gmail.com>
2024-12-03 16:40:01 +0300

    added_player_and_up_key

--- turtle_street_crossing_game/player.py
+++ turtle_street_crossing_game/player.py
@@ -1,15 +1,17 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
-    ken = Turtle("turtle")
-    ken.hideturtle()
-    ken.penup()
-    ken.setheading(90)
-    ken.goto(STARTING_POSITION)
-    ken.showturtle()
-    pass
+    def __init__(self):
+        self.ken = Turtle("turtle")
+        self.ken.hideturtle()
+        self.ken.penup()
+        self.ken.setheading(90)
+        self.ken.goto(STARTING_POSITION)
+        self.ken.showturtle()
 
+    def move_up(self):
+        self.ken.forward(MOVE_DISTANCE)
