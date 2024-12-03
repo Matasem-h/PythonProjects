3c76607188ecfc884004e3e1f8cd3283d87153a6
Matasem <Matasem.h97@gmail.com>
2024-12-03 16:22:46 +0300

    sss

--- turtle_street_crossing_game/player.py
+++ turtle_street_crossing_game/player.py
@@ -1,7 +1,15 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
+    ken = Turtle("turtle")
+    ken.hideturtle()
+    ken.penup()
+    ken.setheading(90)
+    ken.goto(STARTING_POSITION)
+    ken.showturtle()
     pass
+
