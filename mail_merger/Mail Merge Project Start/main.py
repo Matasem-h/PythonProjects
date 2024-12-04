ea1b54c991371bfa2983ceeb4db2dace0102a12d
Matasem <Matasem.h97@gmail.com>
2024-12-04 19:57:26 +0300

    sss

--- mail_merger/Mail Merge Project Start/main.py
+++ mail_merger/Mail Merge Project Start/main.py
@@ -1,3 +1,8 @@
+PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as names_file:
-    names = names_file.read()
+    names = names_file.readlines()
     print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    
