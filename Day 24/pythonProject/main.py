7b6f18f7b2a958fbeb3a46f4fbe31ae694e34581
Matasem <Matasem.h97@gmail.com>
2024-12-04 19:06:48 +0300

    sss

--- Day 24/pythonProject/main.py
+++ Day 24/pythonProject/main.py
@@ -1,9 +1,3 @@
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
-
-# with open("my_file.txt", mode="a") as file:
-#     file.write("\n New text.")
-
-with open("new_file.txt", mode="r,a,w") as file:
-    file.write("\n New text.")
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
