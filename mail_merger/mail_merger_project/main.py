01f849330f1a75b785ece54fa99fdc39903c2121
Matasem <Matasem.h97@gmail.com>
2024-12-04 20:00:12 +0300

    sss

--- mail_merger/mail_merger_project/main.py
+++ mail_merger/mail_merger_project/main.py
@@ -5,4 +5,5 @@ with open("./Input/Names/invited_names.txt") as names_file:
     print(names)
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
-    
+    letter_contents = letter_file.read()
+    print(letter_contents)
