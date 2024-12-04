0d2ad8801fde7986e0cd4ba67c3a687805125827
Matasem <Matasem.h97@gmail.com>
2024-12-04 16:53:29 +0300

    sss

--- Day 24/pythonProject/main.py
+++ Day 24/pythonProject/main.py
@@ -1,1 +1,9 @@
-print("hello")
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\n New text.")
+
+with open("new_file.txt", mode="r,a,w") as file:
+    file.write("\n New text.")
