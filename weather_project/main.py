d9611bedeb9705191047ab0188fea94963836465
Matasem <Matasem.h97@gmail.com>
2024-12-06 20:23:09 +0300

    sss

--- weather_project/main.py
+++ weather_project/main.py
@@ -1,3 +1,23 @@
-with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# temp_avg = sum(temp_list) / len(temp_list)
+# print(temp_avg)
+#
+# temp_max = data["temp"].max()
+# print(temp_max)
+#
+#
+# print(data["condition"])
+# print(data.condition)
+
+data[data.day == "Monday"["day"]]
