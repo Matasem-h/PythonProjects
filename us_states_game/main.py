8eb73242de4531384d622c28a256d2127498595b
Matasem <Matasem.h97@gmail.com>
2024-12-16 21:15:52 +0300

    sss

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -4,25 +4,12 @@ screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
 
+def get_mouse_click_coor(x, y):
+    print(x, y)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen.exitonclick()
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
