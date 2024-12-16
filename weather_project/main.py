30ffde6de8d0ef38d21c573c23024cedbdfef1bd
Matasem <Matasem.h97@gmail.com>
2024-12-16 20:46:14 +0300

    sss

--- weather_project/main.py
+++ weather_project/main.py
@@ -1,38 +1,44 @@
 import pandas
 
-# data = pandas.read_csv("weather_data.csv")
-# # print(type(data))
-# # print(type(data["temp"]))
-
-# data_dict = data.to_dict()
-# print(data_dict)
+# # data = pandas.read_csv("weather_data.csv")
+# # # print(type(data))
+# # # print(type(data["temp"]))
 #
-# temp_list = data["temp"].to_list()
-# print(temp_list)
+# # data_dict = data.to_dict()
+# # print(data_dict)
+# #
+# # temp_list = data["temp"].to_list()
+# # print(temp_list)
+# #
+# # temp_avg = sum(temp_list) / len(temp_list)
+# # print(temp_avg)
+# #
+# # temp_max = data["temp"].max()ss
+# # print(temp_max)
+# #
+# #
+# # print(data["condition"])
+# # print(data.condition)
 #
-# temp_avg = sum(temp_list) / len(temp_list)
-# print(temp_avg)
+# # print(data[data.day == "Monday"])
+# # print(data[data.temp == data.temp.max()])
 #
-# temp_max = data["temp"].max()ss
-# print(temp_max)
+# # monday = data[data.day == "Monday"]
+# # monday_temp = monday.temp[0]
+# # monday_temp_F = monday_temp * 9/5 + 32
+# # print(monday_temp_F)
 #
 #
-# print(data["condition"])
-# print(data.condition)
-
-# print(data[data.day == "Monday"])
-# print(data[data.temp == data.temp.max()])
-
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
-# monday_temp_F = monday_temp * 9/5 + 32
-# print(monday_temp_F)
+# # Creating a dataframe
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
 
 
-# Creating a dataframe
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+
+
+
+
