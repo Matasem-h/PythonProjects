4cc401ed3812d1a207e99e0ab848f3089f8d9c9d
Matasem <Matasem.h97@gmail.com>
2024-12-16 21:19:15 +0300

    ssss

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -6,9 +6,31 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+print(answer_state)
+
+
+
+
+
+
 
 def get_mouse_click_coor(x, y):
     print(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 turtle.onscreenclick(get_mouse_click_coor)
