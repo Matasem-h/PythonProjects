3f5b85d3be791d7c75d7ce1b8581cc3ff8bef495
Matasem <Matasem.h97@gmail.com>
2024-12-16 21:49:26 +0300

    completed_states_guessing_game

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -16,6 +16,12 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="Try to guess all the states!").title()
 
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -27,6 +33,7 @@ while len(guessed_states) < 50:
         t.write(answer_state)
 
 
+# States to learn
 def get_mouse_click_coor(x, y):
     print(x, y)
 
