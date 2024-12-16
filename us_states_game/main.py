8bd7a1f32118161e7404e690893519948810698e
Matasem <Matasem.h97@gmail.com>
2024-12-16 21:36:19 +0300

    ssss

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -8,11 +8,7 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-
-
-
-
-
+all_states = data.state.to_list()
 
 
 
@@ -23,22 +19,19 @@ print(answer_state)
 
 
 
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(state_data.x.item(), state_data.y.item())
+    t.write(answer_state)
 
 
 
 
 def get_mouse_click_coor(x, y):
     print(x, y)
-
-
-
-
-
-
-
-
-
-
 
 
 
