faf319b1d5580e8e85a237e5867b9d234776c8b7
Matasem <Matasem.h97@gmail.com>
2024-12-22 21:41:49 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -23,6 +23,8 @@
 # Dictionary Comprehension from another dictionary
 # new_dict = {new_key: new_value for (key, value) in dict.items() if test}
 
-
-
+# import random
+# names = ['Alex', 'Beth', 'Caroline', 'Dave']
+# students_scores = {student:random.randint(1, 100) for student in names}
+# passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
 
