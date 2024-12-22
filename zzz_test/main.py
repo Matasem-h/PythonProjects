5320889117f9016038e2e7bdb67d8d0429717018
Matasem <Matasem.h97@gmail.com>
2024-12-22 21:21:37 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -1,15 +1,13 @@
 # This is a simple page to test out new stuff that I'm learning.
 
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# squared_numbers = [n ** 2 for n in numbers]
+# print(squared_numbers)
 
-
-
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_numbers = [n ** 2 for n in numbers]
-print(squared_numbers)
-
-
-
-
+# list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+# numbers = [int(num) for num in list_of_strings]
+# result = [num for num in numbers if num % 2 == 0]
+# print(result)
 
 
 
