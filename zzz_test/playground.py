8c53f194c6fce1d386025bed9c62f36eca154185
Matasem <Matasem.h97@gmail.com>
2024-12-28 21:22:54 +0300

    sss

--- zzz_test/playground.py
+++ zzz_test/playground.py
@@ -5,4 +5,30 @@ def add(*args):
     return sum
 
 
-print(add(3, 5, 6))
+# print(add(3, 5, 6))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items()
+        # print(key)
+        # print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
