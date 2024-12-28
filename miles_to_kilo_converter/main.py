19f4e26507d095520e9d931eaeffc03346cefced
Matasem <Matasem.h97@gmail.com>
2024-12-28 23:42:16 +0300

    sss

--- miles_to_kilo_converter/main.py
+++ miles_to_kilo_converter/main.py
@@ -3,17 +3,24 @@ from tkinter import *
 window = Tk()
 window.title("Miles to Kilometer Converter")
 
-miles_input = Entry()
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+window.config(padx=20, pady=20)
 
 miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
 is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
 
 kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
 
 kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
 
 calculate_button = Button(text="Calculate")
+calculate_button.grid(column=1, row=2)
 
 
 
