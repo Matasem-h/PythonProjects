95b3ac55f31594196dd2bf34cfaddbb4ce692743
Matasem <Matasem.h97@gmail.com>
2024-12-28 23:26:09 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -33,6 +33,7 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=100)
 
 
 def button_clicked():
@@ -44,28 +45,22 @@ def button_clicked():
 # Label
 my_label = Label(text="I Am a Label", font=("Arial", 24, "italic"))
 my_label.config(text="New Text")
-# my_label.pack()
 my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 
 # Button
 button = Button(text="Click Me", command=button_clicked)
 button.grid(column=1, row=1)
 
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
 
 # Entry
 input = Entry(width=10)
 print(input.get())
-input.grid(column=2, row=2)
-
-
-
-
-
-
-
-
-
+input.grid(column=3, row=2)
 
 
 window.mainloop()
