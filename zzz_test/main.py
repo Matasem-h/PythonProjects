171c2db2e9cddb562fdf99138363cffb954eb525
Matasem <Matasem.h97@gmail.com>
2024-12-28 23:20:38 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -34,30 +34,30 @@ window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-# Label
 
-my_label = Label(text="I Am a Label", font=("Arial", 24, "italic"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
-
-# Button
 def button_clicked():
     print("I got clicked")
     new_text = input.get()
     my_label["text"] = new_text
 
 
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "italic"))
+my_label.config(text="New Text")
+# my_label.pack()
+my_label.grid(column=0, row=0)
+
+
+# Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
 
 # Entry
 input = Entry(width=10)
-input.pack()
 print(input.get())
+input.grid(column=2, row=2)
+
 
 
 
