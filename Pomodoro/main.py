f11c7e7a4044fea55b844e2ade9f35ddd4d9e640
Matasem <Matasem.h97@gmail.com>
2025-01-05 23:18:50 +0300

    sss

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -22,6 +22,8 @@ def count_down(count):
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
