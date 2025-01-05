68cb23047564d0386ecf66dd601b7d9d97b3b8ac
Matasem <Matasem.h97@gmail.com>
2025-01-05 22:53:01 +0300

    sss

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -29,5 +29,17 @@ canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+
+
+
+
+
+
 
 window.mainloop()
