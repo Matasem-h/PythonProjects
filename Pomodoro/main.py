ae38d05e40532ad4f63b3cbb8f3a5490faf402b2
Matasem <Matasem.h97@gmail.com>
2025-01-05 23:32:15 +0300

    sss

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -13,6 +13,17 @@ reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+
+
+
+
+
+
+
+
+
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -45,6 +56,12 @@ def count_down(count):
         window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,7 +84,7 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
