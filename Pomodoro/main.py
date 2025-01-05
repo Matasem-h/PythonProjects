dcd3c277faee23dc568fdff52309647aa1499b05
Matasem <Matasem.h97@gmail.com>
2025-01-05 22:49:24 +0300

    sss

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -20,11 +20,14 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN)
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 
 window.mainloop()
