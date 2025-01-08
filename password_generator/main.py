5e6ec66911bd22a752c63a4cbb4a6b73c3d89e6f
Matasem <Matasem.h97@gmail.com>
2025-01-08 12:28:18 +0300

    sss

--- password_generator/main.py
+++ password_generator/main.py
@@ -23,11 +23,20 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1 )
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+entries_width = 35
+website_entry = Entry(width=entries_width)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=entries_width)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=entries_width)
+password_entry.grid(row=3, column=1, columnspan=2)
+
+# Buttons
+buttons_width = 30
+generate_password_button = Button(text="Generate Password", width=buttons_width)
+generate_password_button.grid(row=4, column=1)
+add_button = Button(text="Add", width=buttons_width)
+add_button.grid(row=5, column=1, columnspan=2)
+
 
 window.mainloop()
