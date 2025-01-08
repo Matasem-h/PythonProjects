d3f2f1a004c59a0f7600cf14002a94163ad12504
Matasem <Matasem.h97@gmail.com>
2025-01-08 12:01:25 +0300

    sss

--- password_generator/main.py
+++ password_generator/main.py
@@ -12,11 +12,14 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
 # Labels
 website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
 password_label = Label(text="Password")
+password_label.grid(row=3, column=0)
 
 window.mainloop()
