fdbd9891563af0e1ca684697066d550c407c0a1c
Matasem <Matasem.h97@gmail.com>
2025-01-12 21:50:56 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -4,14 +4,17 @@
 # squared_numbers = [n ** 2 for n in numbers]
 # print(squared_numbers)
 
+
 # list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
 # numbers = [int(num) for num in list_of_strings]
 # result = [num for num in numbers if num % 2 == 0]
 # print(result)
 
+
 ##################################################################################################################
 # This is a shorter version of the one below.
 # missing_states = [state for state in all_states if state not in guessed_states]
+
 
 # The one below
 # missing_states = []
@@ -20,47 +23,58 @@
 #         missing_states.append(state)
 ##################################################################################################################
 
+
 # Dictionary Comprehension from another dictionary
 # new_dict = {new_key: new_value for (key, value) in dict.items() if test}
+
 
 # import random
 # names = ['Alex', 'Beth', 'Caroline', 'Dave']
 # students_scores = {student:random.randint(1, 100) for student in names}
 # passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
 
-from tkinter import *
+# from tkinter import *
+#
+# window = Tk()
+# window.title("My First GUI Program")
+# window.minsize(width=500, height=300)
+# window.config(padx=100, pady=100)
+#
+#
+# def button_clicked():
+#     print("I got clicked")
+#     new_text = input.get()
+#     my_label["text"] = new_text
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-window.config(padx=100, pady=100)
+# # Label
+# my_label = Label(text="I Am a Label", font=("Arial", 24, "italic"))
+# my_label.config(text="New Text")
+# my_label.grid(column=0, row=0)
+# my_label.config(padx=50, pady=50)
+
+# # Button
+# button = Button(text="Click Me", command=button_clicked)
+# button.grid(column=1, row=1)
+# new_button = Button(text="New Button")
+# new_button.grid(column=2, row=0)
+
+# # Entry
+# input = Entry(width=10)
+# print(input.get())
+# input.grid(column=3, row=2)
+#
+#
+# window.mainloop()
 
 
-def button_clicked():
-    print("I got clicked")
-    new_text = input.get()
-    my_label["text"] = new_text
+# # FileNotFound Error
+# with open("a_file.txt") as file:
+#     file.read()
 
+# # KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
 
-# Label
-my_label = Label(text="I Am a Label", font=("Arial", 24, "italic"))
-my_label.config(text="New Text")
-my_label.grid(column=0, row=0)
-my_label.config(padx=50, pady=50)
-
-
-# Button
-button = Button(text="Click Me", command=button_clicked)
-button.grid(column=1, row=1)
-
-new_button = Button(text="New Button")
-new_button.grid(column=2, row=0)
-
-
-# Entry
-input = Entry(width=10)
-print(input.get())
-input.grid(column=3, row=2)
-
-
-window.mainloop()
+# # IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
