2c215e6ece52ecd69df49668bafdbe9048f3f07a
Matasem <Matasem.h97@gmail.com>
2025-01-14 14:12:00 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -70,8 +70,19 @@
 # FileNotFound Error
 try:
     file = open("a_file.txt")
-except:
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
     file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
 
 
 # # KeyError
