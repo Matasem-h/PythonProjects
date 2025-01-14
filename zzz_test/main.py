1d617f319f236f9601a1401af15cb7dac4183575
Matasem <Matasem.h97@gmail.com>
2025-01-14 14:16:26 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -67,22 +67,32 @@
 # window.mainloop()
 
 
-# FileNotFound Error
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# # try except
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError()
+
+
+# # Raise
+# height = float(input("Height: "))
+# weight = float(input("Weight: "))
+#
+# if height > 3:
+#     raise ValueError("Human heights should not be over 3m.")
+#
+# bmi = weight / height ** 2
+# print(bmi)
 
 
 # # KeyError
