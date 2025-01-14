3db0c731e6d738754414a6004e8be2dadd0cde34
Matasem <Matasem.h97@gmail.com>
2025-01-14 15:32:44 +0300

    sss

--- password_generator/main.py
+++ password_generator/main.py
@@ -46,19 +46,19 @@ def save():
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any empty field.")
     else:
         try:
-            with open("saved_passwords.json", "r") as data_file:
+            with open("data.json", "r") as data_file:
                 # Reading old data
                 data = json.load(data_file)
         except FileNotFoundError:
-            with open("saved_passwords.json", "w") as data_file:
+            with open("data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
         else:
             # Updating old data with new data
             data.update(new_data)
 
-            with open("saved_passwords.json", "w") as data_file:
+            with open("data.json", "w") as data_file:
                 # Saving updated data
-                json.dump(new_data, data_file, indent=4)
+                json.dump(data, data_file, indent=4)
         finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
