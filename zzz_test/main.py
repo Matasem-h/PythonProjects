fb9d8549ee4ea419010b0b2c45da5c7f44b7dab3
Matasem <Matasem.h97@gmail.com>
2025-01-14 14:39:43 +0300

    Update main.py

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -108,15 +108,29 @@
 # print(text + 5)
 
 
-fruits = ["Apple", "Pear", "Orange"]
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
 
-# Catch the exception and make sure the code runs without crashing.
-def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
-try:
-    make_pie(4)
-except IndexError:
-    print("Fruit pie.")
+
+def count_likes(posts):
+
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except KeyError:
+            pass
+
+    return total_likes
+
+
+count_likes(facebook_posts)
+
 
 
