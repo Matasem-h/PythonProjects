bfa71095ef6ec30be2aa75c81e59756643253652
Matasem <Matasem.h97@gmail.com>
2025-01-19 22:40:16 +0300

    sss

--- german_flashcards/main.py
+++ german_flashcards/main.py
@@ -15,11 +15,11 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image)
+known_button = Button(image=check_image, highlightthickness=0)
 known_button.grid(row=1, column=1)
 
 
