d9c01358fe4155f42172ee5d39226811b6045cd1
Matasem <Matasem.h97@gmail.com>
2025-01-19 22:33:36 +0300

    sss

--- german_flashcards/main.py
+++ german_flashcards/main.py
@@ -6,5 +6,9 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0)
 
 window.mainloop()
