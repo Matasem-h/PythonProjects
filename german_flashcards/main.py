5777598ba3d79dc91521bb2bf040d92e5d45218a
Matasem <Matasem.h97@gmail.com>
2025-01-19 22:34:17 +0300

    sss

--- german_flashcards/main.py
+++ german_flashcards/main.py
@@ -9,6 +9,7 @@ window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0)
 
 window.mainloop()
