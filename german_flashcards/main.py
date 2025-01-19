0726de790f736f7ad294ab9bfad26ef816da343b
Matasem <Matasem.h97@gmail.com>
2025-01-19 23:04:40 +0300

    sss

--- german_flashcards/main.py
+++ german_flashcards/main.py
@@ -6,21 +6,34 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/german_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="German")
-    canvas.itemconfig(card_word, text=current_card["German"])
+    canvas.itemconfig(card_title, text="German", fill="black")
+    canvas.itemconfig(card_word, text=current_card["German"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
