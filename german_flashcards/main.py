9c64f3a79067a386cba9c999b4a8e3aefd58e867
Matasem <Matasem.h97@gmail.com>
2025-01-19 22:48:05 +0300

    sss

--- german_flashcards/main.py
+++ german_flashcards/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -8,7 +9,9 @@ to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    pass
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="German")
+    canvas.itemconfig(card_word, text=current_card["German"])
 
 
 window = Tk()
@@ -18,8 +21,8 @@ window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
