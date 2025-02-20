176ea1b7a10fd939e1cf50a7b8a325e8679af6ee
Matasem <Matasem.h97@gmail.com>
2025-02-20 15:21:44 +0300

    sss

--- Day33/main.py
+++ Day33/main.py
@@ -1,4 +1,7 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+if response.status_code == 404:
+    raise Exception("That resource does not exist.")
+elif response.status_code == 401:
+    raise Exception("You are not allowed to access this data.")
