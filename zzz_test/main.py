854dd688afd7bc0fc8eeee89e78f091b5d9eb385
Matasem <Matasem.h97@gmail.com>
2025-02-20 15:47:33 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -106,3 +106,19 @@
 # # TypeError
 # text = "abc"
 # print(text + 5)
+
+
+# requests
+import requests
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
