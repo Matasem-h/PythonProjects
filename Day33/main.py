6c4f80afa6321cd8f13b27d6163b510ef8834a08
Matasem <Matasem.h97@gmail.com>
2025-02-20 15:26:54 +0300

    sss

--- Day33/main.py
+++ Day33/main.py
@@ -1,7 +1,12 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-if response.status_code == 404:
-    raise Exception("That resource does not exist.")
-elif response.status_code == 401:
-    raise Exception("You are not allowed to access this data.")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
