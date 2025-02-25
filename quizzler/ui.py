0304f35a5921aab19161b262c0e7da74f189a13e
Matasem <Matasem.h97@gmail.com>
2025-02-25 19:31:09 +0300

    sss

--- quizzler/ui.py
+++ quizzler/ui.py
@@ -7,6 +7,15 @@ class QuizzInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2)
+
 
 
         self.window.mainloop()
