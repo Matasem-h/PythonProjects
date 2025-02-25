5b7fd94828b2ba984609382c6fba7b493947e837
Matasem <Matasem.h97@gmail.com>
2025-02-25 19:37:52 +0300

    sss

--- quizzler/ui.py
+++ quizzler/ui.py
@@ -20,8 +20,14 @@ class QuizzInterface:
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
 
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
