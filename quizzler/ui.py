b88ce821b498f988abe95952108ef704d7979c52
Matasem <Matasem.h97@gmail.com>
2025-02-25 19:24:19 +0300

    sss

--- quizzler/ui.py
+++ quizzler/ui.py
@@ -1,2 +1,12 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizzInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+
+        self.window.mainloop()
