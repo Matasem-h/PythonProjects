29cedf9fdde2e1979c11d62b44cdc82b1d429dbf
Matasem <Matasem.h97@gmail.com>
2025-02-27 15:24:22 +0300

    sss

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -13,4 +13,13 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("⛈️ It is going to rain! ⛈️")
+
