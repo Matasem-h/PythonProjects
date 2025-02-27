4fe2d1c34ae3ab080d3edb8f48f4314d0177227a
Matasem <Matasem.h97@gmail.com>
2025-02-27 15:02:39 +0300

    sss

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,1 +1,13 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
 api_key = "0aa8d86adf6eda1f4c445b3798580c1e"
+
+weather_params = {
+    "lat": 21.485811,
+    "lon": 39.192505,
+    "appid": api_key,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code)
