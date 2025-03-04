712ad15dcf1ccb446db6ccdfdb5bc128a1c99699
Matasem <Matasem.h97@gmail.com>
2025-03-04 23:34:10 +0300

    finished_tesla_last_two_days_stock_difference

--- stock_news_email/main.py
+++ stock_news_email/main.py
@@ -9,10 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = "BNYPXRL14R9ABS64"
 NEWS_API_KEY = "0eb59f966d07473faf7bbc07e5fe944a"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-#Get yesterday's closing stock price.
+# Get yesterday's closing stock price.
 stock_param = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
@@ -26,16 +24,17 @@ yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 print(yesterday_closing_price)
 
-#Get the day before yesterday's closing stock price.
+# Get the day before yesterday's closing stock price.
 day_before_yesterday = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday["4. close"]
 print(day_before_yesterday_closing_price)
 
-#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
-#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+# Work out the percentage difference in price between closing price yesterday
+# closing price the day before yesterday.
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
@@ -49,28 +48,6 @@ if diff_percent > 2:
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# Use Python slice operator to create a list that contains the first 3 articles.
     three_articles = articles[:3]
     print(three_articles)
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
