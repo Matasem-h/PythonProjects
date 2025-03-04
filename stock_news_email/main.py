d4be273fdd0e8bdcaa27ca7fb986a18cbade5236
Matasem <Matasem.h97@gmail.com>
2025-03-04 23:20:37 +0300

    sss

--- stock_news_email/main.py
+++ stock_news_email/main.py
@@ -39,6 +39,8 @@ diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if diff_percent > 2:
+    print("Get News")
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
