5324a28652266b70e1c18313b511807122d35c0a
Matasem <Matasem.h97@gmail.com>
2025-03-04 23:28:35 +0300

    sss

--- stock_news_email/main.py
+++ stock_news_email/main.py
@@ -39,9 +39,16 @@ print(difference)
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+# If TODO4 percentage is greater than 5 then print("Get News").
 if diff_percent > 2:
-    print("Get News")
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
