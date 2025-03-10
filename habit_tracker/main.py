470230faf9f04910d43eb296b23aaeeb930fd102
Matasem <Matasem.h97@gmail.com>
2025-03-10 22:48:27 +0300

    sss

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,10 +1,13 @@
 import requests
 
+USERNAME = "matasem"
+TOKEN = "juifwe345ijokklmdfs43klm5"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_param = {
-    "token": "juifwe345ijokklmdfs43klm5",
-    "username": "matasem",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
@@ -13,3 +16,19 @@ user_param = {
 # response = requests.post(url=pixela_endpoint, json=user_param)
 # print(response.text)
 
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Push-ups Graph",
+    "unit": "commit",
+    "type": "int",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
