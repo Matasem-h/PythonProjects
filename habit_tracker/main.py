026fc162df9ec735f5ef2f30c8433a658fad92f6
Matasem <Matasem.h97@gmail.com>
2025-03-10 23:11:35 +0300

    sss

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -38,14 +38,24 @@ headers = {
 
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-today = datetime(year=2025, month=3, day=9)
-print(today.strftime("%Y%m%d"))
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
 
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
-    "quantity": "30",
+    "quantity": input("How many Push-ups have you done?\n"),
 }
 
 # response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 # print(response.text)
 
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+new_pixel_data = {
+    "quantity": "7",
+}
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+# response = requests.delete(url=delete_endpoint,headers=headers)
+# print(response.text)
