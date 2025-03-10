dc35ef319e9aaafcbaa812a32cd20338cf21dd49
Matasem <Matasem.h97@gmail.com>
2025-03-10 22:38:28 +0300

    sss

---
+++ habit_tracker/main.py
@@ -0,0 +1,15 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token": "juifwe345ijokklmdfs43klm5",
+    "username": "matasem",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# https://pixe.la/@matasem - My account
+
+# response = requests.post(url=pixela_endpoint, json=user_param)
+# print(response.text)
+
