079e68265b04b0eb70f307d2ad96781146c392ca
Matasem <Matasem.h97@gmail.com>
2025-03-22 23:26:57 +0300

    added_turtle_first_drawing

---
+++ first_turtle_drawing_/main.py
@@ -0,0 +1,52 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+turtle.colormode(255)
+
+for _ in range(3):
+    tim.forward(100)
+    tim.right(360 / 3)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(360 / 4)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(5):
+    tim.forward(100)
+    tim.right(360 / 5)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(6):
+    tim.forward(100)
+    tim.right(360 / 6)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(7):
+    tim.forward(100)
+    tim.right(360 / 7)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(8):
+    tim.forward(100)
+    tim.right(360 / 8)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(9):
+    tim.forward(100)
+    tim.right(360 / 9)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+for _ in range(10):
+    tim.forward(100)
+    tim.right(360 / 10)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+screen = Screen()
+screen.exitonclick()
+
