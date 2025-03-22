02b6a40bbbb6e28c0ddefcfa81929aff1ef39da8
Matasem <Matasem.h97@gmail.com>
2025-03-22 23:29:09 +0300

    sss

--- turtle_first_drawing/main.py
+++ turtle_first_drawing/main.py
@@ -7,6 +7,10 @@ tim.shape("turtle")
 tim.color("green")
 turtle.colormode(255)
 
+for _ in range(3):
+    tim.forward(100)
+    tim.right(360 / 3)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 screen = Screen()
 screen.exitonclick()
