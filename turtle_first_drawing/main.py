700e73879bf273b112785d111b4064cc1b7a0b98
Matasem <Matasem.h97@gmail.com>
2025-03-22 23:29:33 +0300

    sss

--- turtle_first_drawing/main.py
+++ turtle_first_drawing/main.py
@@ -17,6 +17,12 @@ for _ in range(4):
     tim.right(360 / 4)
     tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+for _ in range(5):
+    tim.forward(100)
+    tim.right(360 / 5)
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
 screen = Screen()
 screen.exitonclick()
 
