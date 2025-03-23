cd7efef151ee37e80212c20c47361999c507f8c7
Matasem <Matasem.h97@gmail.com>
2025-03-23 23:41:35 +0300

    sss

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -8,5 +8,8 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color from"
                                                           " (red, orange, yellow, green, blue, purple): ").lower()
 
+print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+random.shuffle(colors)
 
 screen.exitonclick()
