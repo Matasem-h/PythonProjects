ca549503d9895d23f983c16831766585604d121a
Matasem <Matasem.h97@gmail.com>
2025-03-23 23:41:19 +0300

    sss

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -2,5 +2,11 @@ import turtle
 from turtle import Turtle, Screen
 import random
 
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color from"
+                                                          " (red, orange, yellow, green, blue, purple): ").lower()
+
 
 screen.exitonclick()
