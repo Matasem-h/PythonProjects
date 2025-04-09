e39adcf7e1f4443cc547da846a996473120fb521
Matasem <Matasem.h97@gmail.com>
2025-04-09 07:13:51 +0300

    sss

--- beatiful_soap/bs4-start/main.py
+++ beatiful_soap/bs4-start/main.py
@@ -10,4 +10,14 @@ soup = BeautifulSoup(contents, "lxml")
 # print(soup.p)
 
 all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
