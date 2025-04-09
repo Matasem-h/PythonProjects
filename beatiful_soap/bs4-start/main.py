1f9bf52644a3902d5d55c00b2357269ccd33cf75
Matasem <Matasem.h97@gmail.com>
2025-04-09 07:05:43 +0300

    sss

--- beatiful_soap/bs4-start/main.py
+++ beatiful_soap/bs4-start/main.py
@@ -2,9 +2,12 @@ from bs4 import BeautifulSoup
 import lxml
 
 with open("website.html") as file:
-    content = file.read()
+    contents = file.read()
 
-soup = BeautifulSoup(content, "lxml")
+soup = BeautifulSoup(contents, "lxml")
 # print(soup.title.string)
 # print(soup.a)
 # print(soup.p)
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
