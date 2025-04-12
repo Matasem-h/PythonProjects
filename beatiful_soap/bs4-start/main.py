d28c78ec613c34d1d969a434d0a3758a0f19b653
Matasem <Matasem.h97@gmail.com>
2025-04-12 22:18:24 +0300

    sss

--- beatiful_soap/bs4-start/main.py
+++ beatiful_soap/bs4-start/main.py
@@ -5,8 +5,8 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
-print(soup.title)
-
+article_tag = soup.find(name="a", class_="sitebit comhead")
+print(article_tag.getText())
 
 
 
