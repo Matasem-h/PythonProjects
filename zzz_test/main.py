76e01b68cf2193d7b7eeac40b92568163856fc51
Matasem <Matasem.h97@gmail.com>
2025-04-12 17:14:49 +0300

    sss

--- zzz_test/main.py
+++ zzz_test/main.py
@@ -109,24 +109,57 @@
 
 
 # requests
-import requests
-from datetime import datetime
+# import requests
+# from datetime import datetime
+#
+# MY_LAT = 51.507351
+# MY_LONG = -0.127758
+#
+# parameters = {
+#     "lat": MY_LAT,
+#     "lng": MY_LONG,
+#     "formatted": 0
+# }
+#
+# response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+# response.raise_for_status()
+# data = response.json()
+# sunrise = data["results"]["sunrise"]
+# sunset = data["results"]["sunset"]
+#
+# print(sunrise)
+# print(sunset)
+# time_now = datetime.now()
 
-MY_LAT = 51.507351
-MY_LONG = -0.127758
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0
-}
+# Soup???
+# from bs4 import BeautifulSoup
+# import lxml
+#
+# with open("website.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "lxml")
+# # print(soup.title.string)
+# # print(soup.a)
+# # print(soup.p)
+#
+# all_anchor_tags = soup.find_all(name="a")
+# # print(all_anchor_tags)
+#
+# # for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     # print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+#
+# name = soup.select_one(selector="name")
+# print(name)
+#
+# soup.select(".heading")
+# print(heading)
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-
-print(sunrise)
-print(sunset)
-time_now = datetime.now()
