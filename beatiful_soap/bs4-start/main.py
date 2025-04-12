12bacc32f1b77a6516f934776d62708969567c6f
Matasem <Matasem.h97@gmail.com>
2025-04-12 17:41:10 +0300

    sss

---
+++ beatiful_soap/bs4-start/main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+print(soup.title)
+
+
+
+
+
+
+
+
+
+
+
