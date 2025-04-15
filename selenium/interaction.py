2ce84c4f0aa88db5580a666693c08e7ed9bf5b39
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:54:38 +0300

    sss

--- selenium/interaction.py
+++ selenium/interaction.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # To keep Chrome open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -6,18 +7,15 @@ chrome_options.add_experimental_option("detach", True)
 
 # Create and configure the Chrome webdriver
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 # Navigate to Wikipedia
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-
-
-
-
-
-
+# Hone in on anchor tag using CSS selectors
+article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+print(article_count.text)
 
 
 # To close Chrome after program finishes
 # driver.close()
-# driver.quit()
+driver.quit()
