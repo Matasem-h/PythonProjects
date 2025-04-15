a60883829ed6145785cd050c2ae18215eecd5bbe
Matasem <Matasem.h97@gmail.com>
2025-04-15 23:01:30 +0300

    sss

--- selenium/interaction.py
+++ selenium/interaction.py
@@ -13,9 +13,21 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 # Hone in on anchor tag using CSS selectors
 article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(article_count.text)
+# article_count.click()
+# print(article_count.text)
+
+# Find element by Link Text
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# Find the "Search" <input> by Name
+search = driver.find_element(By.NAME, value="search")
+
+# Sending keyboard input to Selenium
+search.send_keys("Python")
+
 
 
 # To close Chrome after program finishes
 # driver.close()
-driver.quit()
+# driver.quit()
