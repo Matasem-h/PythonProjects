1b88f8faf8ca10c353d4046a81c46441dc4ee51f
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:02:59 +0300

    sss

---
+++ day_48/main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+# To keep Chrome open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+
+
+
+
+
+
+
+
+
+
