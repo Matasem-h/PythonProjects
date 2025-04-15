a8ab65692336a6927cc72aa088e4823f5f4014bc
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:25:42 +0300

    sss

--- day_48/main.py
+++ day_48/main.py
@@ -7,14 +7,21 @@ chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://appbrewery.github.io/instant_pot/")
+driver.get("https://www.python.org/")
 
-price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"The price is {price_dollar.text}.{price_cents.text}")
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.get_attribute("placeholder"))
+button = driver.find_element(By.ID, value="submit")
+print(button.size)
+documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+print(documentation_link.text)
 
 # driver.close()
-driver.quit()
+# driver.quit()
 
 
 # https://appbrewery.github.io/instant_pot/
