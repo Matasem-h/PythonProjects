6e030a2c1f46701b50d42d69f6a1574bba71cecf
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:41:46 +0300

    sss

--- selenium/main.py
+++ selenium/main.py
@@ -17,17 +17,28 @@ driver.get("https://www.python.org/")
 # print(search_bar.get_attribute("placeholder"))
 # button = driver.find_element(By.ID, value="submit")
 # print(button.size)
+
+# By.CSS_SELECTOR
 # documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
 # print(documentation_link.text)
 
+# By.XPath
 # bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
 # print(bug_link.text)
 
-driver.find_elements(By.CSS_SELECTOR)
+# Finding many elements
+# driver.find_elements(By.CSS_SELECTOR)
 
+
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+for time in event_times:
+    print(time.text)
+
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+for name in event_names:
+    print(name.text)
+
+
+# Close Chrome
 # driver.close()
-# driver.quit()
-
-
-# https://appbrewery.github.io/instant_pot/
-# https://www.amazon.com
+driver.quit()
