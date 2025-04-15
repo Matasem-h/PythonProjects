6de48f12e1b621f38e8bfe94d36462ac102a78f2
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:12:58 +0300

    sss

--- day_48/main.py
+++ day_48/main.py
@@ -7,13 +7,15 @@ chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get("https://appbrewery.github.io/instant_pot/")
 
 price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
 price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"The price is {price_dollar.text}.{price_cents.text}")
 
 # driver.close()
-# driver.quit()
+driver.quit()
 
 
-
+# https://appbrewery.github.io/instant_pot/
+# https://www.amazon.com
