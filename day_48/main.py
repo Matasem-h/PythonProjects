4ad2fbbd4e70c1e01cd0fec0e1afe406726dbeb8
Matasem <Matasem.h97@gmail.com>
2025-04-15 22:30:28 +0300

    sss

--- day_48/main.py
+++ day_48/main.py
@@ -13,12 +13,17 @@ driver.get("https://www.python.org/")
 # price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
 # print(f"The price is {price_dollar.text}.{price_cents.text}")
 
-search_bar = driver.find_element(By.NAME, value="q")
-print(search_bar.get_attribute("placeholder"))
-button = driver.find_element(By.ID, value="submit")
-print(button.size)
-documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
-print(documentation_link.text)
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+# button = driver.find_element(By.ID, value="submit")
+# print(button.size)
+# documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(documentation_link.text)
+
+# bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+driver.find_elements(By.CSS_SELECTOR)
 
 # driver.close()
 # driver.quit()
