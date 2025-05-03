8f7313eef4c8847578537a954e05678d323e99de
Matasem <Matasem.h97@gmail.com>
2025-05-03 23:23:43 +0300

    sss

--- my_personal_site/server.py
+++ my_personal_site/server.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("angela.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
