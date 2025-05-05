047c9241149ec4e7d0015e0604b47abce09cb565
Matasem <Matasem.h97@gmail.com>
2025-05-06 01:00:28 +0300

    sss

--- day-57/server.py
+++ day-57/server.py
@@ -16,11 +16,17 @@ def home():
 
 @app.route("/guess/<name>")
 def guess(name):
-    gender_url = f"https://api.agify.io?name={name}"
+    gender_url = f" https://api.genderize.io?name={name}"
     gender_response = requests.get(gender_url)
-    
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
 
-    return render_template("guess.html", person_name=name)
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+
+    return render_template("guess.html", person_name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
