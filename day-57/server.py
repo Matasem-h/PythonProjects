ea46e67bc571422653bdb3c4c88f2163f79854cd
Matasem <Matasem.h97@gmail.com>
2025-05-06 00:35:38 +0300

    sss

--- day-57/server.py
+++ day-57/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import random
+import datetime
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=current_year)
 
 
 if __name__ == "__main__":
