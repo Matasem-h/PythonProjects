2f3797ada010f9cad822c33b035cbede7f0bd811
Matasem <Matasem.h97@gmail.com>
2025-05-06 00:49:44 +0300

    sss

--- day-57/server.py
+++ day-57/server.py
@@ -12,9 +12,10 @@ def home():
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
 
+
 @app.route("/guess/<name>")
 def guess(name):
-
+    return render_template("guess.html", person_name=name)
 
 
 if __name__ == "__main__":
