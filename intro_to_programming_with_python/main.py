f9fc71582be48803ae3fefd8d74387169f6e92c2
Matasem <Matasem.h97@gmail.com>
2026-02-17 15:50:43 +0300

    sss

--- intro_to_programming_with_python/main.py
+++ intro_to_programming_with_python/main.py
@@ -1,7 +1,5 @@
 # Unit 1 Code
-import jupyter
-
-print("Hello, World!")       # Hello, World! In Python
+# print("Hello, World!")       # Hello, World! In Python
 
 
 
@@ -31,13 +29,17 @@ print("Hello, World!")       # Hello, World! In Python
 
 
 # Unit 2 Code
+# my_file = open("my_file.txt", "w")
+# my_file.write("I am writing to a file in Python!!!")
+# my_file.close()
+
+# my_file = open("my_file.txt", "r")
+# print(my_file.read())
+# my_file.close()
 
 
-
-
-
-
-
+my_string = "He said 'Hi'"
+print(my_string)
 
 
 
